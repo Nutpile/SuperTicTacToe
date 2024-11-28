c82789ab8e58a4ed4d8f25a3e7693506ff6e6b83
Nutpile <nutpile@hotmail.com>
2024-11-28 18:55:28 +0100

    Added the main grid

--- src/main.c
+++ src/main.c
@@ -1,10 +1,50 @@
 #include <graphx.h>
 #include <ti/getcsc.h>
 
+//screen dimensions are 320, 240, with midpoint being 160, 120
+
+void init()
+{
+    int cellsize = 25; // Size of each small cell
+    int height = 8;    // Starting position (top-left corner)
+
+    // Draw horizontal lines
+    for (int i = height; i <= cellsize * 9 + height; i += cellsize)
+    {
+        // Check if this is a "big grid" line
+        if ((i - height) % (cellsize * 3) == 0)
+        {
+            gfx_SetColor(224); // Red for big grid lines
+        }
+        else
+        {
+            gfx_SetColor(0); // Black for small grid lines
+        }
+        gfx_HorizLine_NoClip(height, i, cellsize * 9 + 1);
+    }
+
+    // Draw vertical lines
+    for (int i = height; i <= cellsize * 9 + height; i += cellsize)
+    {
+        // Check if this is a "big grid" line
+        if ((i - height) % (cellsize * 3) == 0)
+        {
+            gfx_SetColor(224); // Red for big grid lines
+        }
+        else
+        {
+            gfx_SetColor(0); // Black for small grid lines
+        }
+        gfx_VertLine_NoClip(i, height, cellsize * 9);
+    }
+}
+
 
 int main()
 {
     gfx_Begin();
-    while(os_GetCSC());
+    init();
+    while(!os_GetCSC()){}
+    gfx_End();
     return 0;
 }
